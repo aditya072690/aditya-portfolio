b65e9fcfd0e2724d07e6040a94c150b457d6b509
Aditya Chauhan <chauhanaditya072@gmail.com>
2025-02-27 11:25:21 +0530

    edite file

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span className="pl-12 pb-4">
-          <Image src="/images/logoblack.svg" width={75} height={50} alt="logo"  />
+          <Image src="/images/Logoblack.svg" width={75} height={50} alt="logo"  />
         </span>
         <p className="text-slate-600 pt-5">All rights reserved.</p>
       </div>
